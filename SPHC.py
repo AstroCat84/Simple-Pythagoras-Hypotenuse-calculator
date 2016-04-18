f8bccee753f82fce18f16797d84ef54c38b11b4e
Astrocat84 <brohit23@outlook.com>
2016-04-18 22:28:19 +0530

    Added files via upload

---
+++ SPHC.py
@@ -0,0 +1,11 @@
+#Pythagoras' theorem
+
+a = float(input("Enter 1st length: "))
+b = float(input("Enter 2nd length: "))
+
+c_squared = a**2 + b**2
+
+c = c_squared**0.5
+
+print("The length of the hypotenuse is ", c)
+input("Press close to exit")
